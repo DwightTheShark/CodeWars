389f5c8ab20cb1ce62fc47384332a3cafcf2c059
AdoraNicole <86696232+DwightTheShark@users.noreply.github.com>
2022-09-17 16:08:24 -0700

    Add files via upload

---
+++ 22-09-17.js
@@ -0,0 +1,9 @@
+// the array so that the animal is the right way round (head, body, tail).
+
+// Same goes for all the other arrays/lists that you will get in the tests: you have to change the element positions with the same exact logics
+
+// Simples!
+
+function fixTheMeerkat(arr) {
+  return arr.reverse();
+}
